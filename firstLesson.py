20caeda36f7555b26bb06da11766697d054508f0
Wassim <root1@MacBook-Engneering-waseem.local>
2019-08-20 22:07:34 +0300

    First Lesson - Week one

---
+++ firstLesson.py
@@ -0,0 +1,10 @@
+# the file wrote by Engineer WaseemDayili
+
+myName = 'Waseem Dayili'
+nickName = 'Loyal'
+
+if myName == 'Waseem Dayili':
+	print('Right, you are my boyfriend...')
+else:
+	print('ohh, you are not....')
+
