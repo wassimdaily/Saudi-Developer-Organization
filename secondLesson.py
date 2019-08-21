0789f355777c4dda939c489eccca36d81493c99b
Wassim <root1@MacBook-Engneering-waseem.local>
2019-08-21 23:37:58 +0300

    seond day

---
+++ secondLesson.py
@@ -0,0 +1,16 @@
+# "My name is waseem, this is my second day of 100 to learn python language"
+"""
+
+My name is wassim, I'm 21 old.
+I'm study computer and engineering
+
+that set
+
+"""
+# my old code
+# myName = 'Waseem'
+
+newName = 'Waseem Dayili'
+
+
+print(newName)
