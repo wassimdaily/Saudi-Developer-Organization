d35c45b2f99e0a47ea3161e8317ebdc72837b920
= <root1@MacBook-Engneering-waseem.local>
2019-08-23 02:56:39 +0300

    versionWeekOne

---
+++ Week1/QuizLesson.py
@@ -0,0 +1,7 @@
+# Quiz of the last lesson of the first week.
+
+# variables to store the values
+
+x,y,z = 'apple', 'orange', 'limon'
+
+print('There are three things are: ' +x+", "+y+", "+z)
---
+++ Week1/fourthLesson.py
@@ -0,0 +1,28 @@
+import random
+
+# three types in python
+
+
+x = 1
+y = 2.5
+z = 100j
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+cX = float(x)
+cY = complex(y)
+cZ = int(y)
+ 
+print( cX )
+print( cY )
+print( cZ )
+
+randNum = random.randrange(1,10)
+
+print(randNum)
+print(randNum)
+print(randNum)
+
