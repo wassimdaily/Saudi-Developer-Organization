a74d2ffa61b5fd9fb15347dbcef32e8253c444f5
businesswassim@gmail.com <root1@MacBook-Engneering-waseem.local>
2019-08-25 22:54:31 +0300

    Second-week-update

---
+++ Week2/firstLesson.py
@@ -0,0 +1,16 @@
+# convert values to another it's called casting in the python lang.
+
+
+# integer number to float or double called in the java lang.
+
+print(float(1))
+
+# float number to int called in the java lang.
+
+print(int(233.223))
+
+# convert the numbers to string
+
+print(str(2893223))
+
+
---
+++ Week2/secondLesson.py
@@ -0,0 +1,26 @@
+# Python strings
+
+myName = 'Wassim Dayili'
+myPlace = "USA, Calafornia"
+
+# info about my silf
+
+myStory = """
+My name is wassim. I'm 21 old.
+I live in USA, Calafornia.
+"""
+
+myCollege = '''
+
+My college at Usa.
+
+Yeah that set,
+
+'''
+
+
+print(myName[0:6]) # to print my first name of full name stored in the variable
+print(myPlace)
+print(myStory)
+print(myCollege)
+print(myPlace[0]) # print the first value of the string
