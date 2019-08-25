9af211da4805b96bd2d11e74566992317aeed868
businesswassim@gmail.com <root1@MacBook-Engneering-waseem.local>
2019-08-25 23:15:14 +0300

    third-lesson

---
+++ Week2/thirdLesson.py
@@ -0,0 +1,22 @@
+# to delete whitespace in the python by using strip() function.
+# for example.
+
+myName = '     Waseem Dayili       '
+
+
+print("Without by using strip funtion the result as: " + myName)
+
+print('The result by using strip funtion as: ' + myName.strip())
+
+# to calculate the string content by using len function
+print("There're " + str(len(myName.strip())) + ' letters are there with whitespace.')
+
+# to make the english letter lower by using lower function
+
+print(myName.lower())
+
+# to make the english letter upper by using lower function.
+
+print(myName.upper())
+print(myName.replace('Dayili', 'Ahmed'))
+print(myName.split(' '))
