7566f0c28aad33f3c551105df3f73d0d618ad852
businesswassim@gmail.com <root1@MacBook-Engneering-waseem.local>
2019-09-14 21:59:47 +0300

    week5-verstion

---
+++ Week5/firstLesson.py
@@ -0,0 +1,31 @@
+# if and else, please use python 3
+
+myName = 'waseem'
+myAge = 21
+
+
+if myName == 'waseem' and myAge == 21:
+	print('yes')
+
+elif myAge == 'waseem' and myAge <=21:
+	print('yes')
+else:
+	print('no')
+
+
+if myAge >= 21 and myAge < 22:
+	print('yes he is 21')
+
+if 5<6 :print('yes 5 < 6')
+
+
+print('yes 5 is 5') if 5 < 5 else print('5 is not 5')
+
+
+
+if myName == 'waseem' or myAge == 20:
+	print('yes')
+	if myAge != 20:
+		print('is greater or less then 20')
+	else:
+			print('I can not guess')
